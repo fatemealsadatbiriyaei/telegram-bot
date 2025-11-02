0105c14d29b619fb948241d8d20f1cb41d55c9f0
Fatemeh Sadat Biriyaei <fa671biriyaei@gmail.com>
2025-11-02 09:10:09 +0330

    Initial commit - telegram bot for Render

--- bot.py
+++ bot.py
@@ -2,8 +2,8 @@ import os
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-# توکن از متغیر محیطی خوانده می‌شود (در Render اضافه‌اش می‌کنیم)
 BOT_TOKEN = os.environ["BOT_TOKEN"]
+WEBHOOK_URL = f"https://telegram-bot-1-itzu.onrender.com/{BOT_TOKEN}"  # URL سرویس Render شما
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     await update.message.reply_text("سلام 👋 خوش اومدی به ربات صفر فروش‌ها!")
@@ -11,5 +11,10 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 app = ApplicationBuilder().token(BOT_TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 
-print("ربات فعال شد ✅")
-app.run_polling()
+# Webhook
+app.run_webhook(
+    listen="0.0.0.0",
+    port=int(os.environ.get("PORT", 5000)),  # Render خودش PORT اختصاص میده
+    url_path=BOT_TOKEN,
+    webhook_url=WEBHOOK_URL
+)
